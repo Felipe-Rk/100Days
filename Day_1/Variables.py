859ca2ceebe60b26d88ea800fc3156687ed4bfef
Felipe Roiko Felix da Silva <roiko2010@gmail.com>
2023-09-20 21:22:15 -0300

    first commit

---
+++ Day_1/Variables.py
@@ -0,0 +1,15 @@
+#declaração de varialvel com dado jack
+name = "Jack"
+#impressão de dado da variavel nome
+print(name)
+
+#substituição de dado jack para felipe 
+name = "Felipe"
+print(name)
+
+#variavel com entrada de dados
+name=input("What is your name? ")
+#declaração de nova variavel para ler o tamanho do nome
+length=len(name)
+#impressão de nova variavel de contagem 
+print(length)
---
+++ Day_1/gerador_nome_banda.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Bem vindo ao gerador de nome de banda")
+#2. Ask the user for the city that they grew up in.
+cidade = input("Qual cidade você nasceu? \n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual o nome do seu animal de estimação: \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da sua banda é "+cidade+" "+pet)
+print("Thank you.")
