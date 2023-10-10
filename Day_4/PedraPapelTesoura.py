513b9ac8ad36dea9507111677cdf38230ef7d09f
Felipe Roiko Felix da Silva <roiko2010@gmail.com>
2023-10-10 16:37:37 -0300

    commit

--- Day_4/PedraPapelTesoura.py
+++ Day_4/PedraPapelTesoura.py
@@ -31,25 +31,31 @@ import random
 imagens = [pedra, papel, tesoura]
 
 jogador = int(input("Escolha entre pedra, papel ou tesoura. Pedra = 0 / Papel = 1 / Tesoura = 2: "))
-print("Você escolheu: ", imagens[jogador])
 
-computador_escolha = random.randint(0,2)
-print("O computador escolheu: ",imagens[computador_escolha])
+if jogador >=3:
+    print("Vôce digitou um numero invalido e perdeu")
 
-if jogador >= 3 or jogador < 0:
-    print("Você digitou errado ")
+else:
+    print("Você escolheu: ", imagens[jogador])
 
-elif jogador == 0 and computador_escolha == 2: 
-    print("Você ganhou!!!")
+    computador_escolha = random.randint(0,2)
 
-elif computador_escolha == 0 and jogador == 2:
-    print("Você perdeu!!!")
+    print("O computador escolheu: ",imagens[computador_escolha])
 
-elif jogador > computador_escolha:
-    print("Você ganhou!!!")
+    if jogador >= 3 or jogador < 0:
+        print("Você digitou errado ")
 
-elif computador_escolha > jogador:
-    print("Você perdeu!!!")   
+    elif jogador == 0 and computador_escolha == 2: 
+        print("Você ganhou!!!")
 
-elif jogador == computador_escolha:
-    print("Empate")
+    elif computador_escolha == 0 and jogador == 2:
+        print("Você perdeu!!!")
+
+    elif jogador > computador_escolha:
+        print("Você ganhou!!!")
+
+    elif computador_escolha > jogador:
+        print("Você perdeu!!!")   
+
+    elif jogador == computador_escolha:
+        print("Empate")
